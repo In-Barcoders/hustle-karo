7b031160ac28688c83c30fe6365b004710bc2906
justshivam <stdnt.shivam@gmail.com>
2021-04-28 15:55:07 +0530

    Revert "Fix"
    This reverts commit 78170e458748d8eeba50554694b1a7a2289b5199.

--- justshivam/28-04-21/question2.py
+++ justshivam/28-04-21/question2.py
@@ -1,10 +1,12 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        sign = -1 if ((dividend < 0) ^ (divisor < 0)) else 1
-        dividend = abs(dividend)
-        divisor = abs(divisor)
-        quotient = 0
-        while (dividend >= divisor):
-            dividend -= divisor
-            quotient += 1
-        return sign * quotient
+        if divisor == 0:
+            return 0
+        res = int((divisor**(int((1j**2).real)))*dividend)
+        MAX = 2**31
+        if res > MAX-1:
+            return MAX-1
+        elif res < -MAX:
+            return -MAX
+        else:
+            return res
